639311f3a3c64879dc182b849499b1b6dee2bec6
Ayushi Rhutwik Pohekar <ayushi2021.29@gmail.com>
2023-04-30 01:30:06 +0100

    url changed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import { Provider } from 'react-redux';
 import {store} from './store'
-axios.defaults.baseURL="http://localhost:8010"
+axios.defaults.baseURL="https://blogapp-s2xm.onrender.com"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
